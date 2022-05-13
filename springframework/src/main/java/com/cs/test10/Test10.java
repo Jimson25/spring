20c91b7eef6b1a853f2b40ece2f9aa7b686a9778
Jim <248578737@qq.com>
2022-05-13 23:28:27 +0800

    spring 整合junit4

--- springframework/src/main/java/com/cs/test10/Test10.java
+++ springframework/src/main/java/com/cs/test10/Test10.java
@@ -18,7 +18,6 @@ public class Test10 {
 
     @Test
     public void test01() {
-        // public class AnnotationConfigApplicationContext extends GenericApplicationContext
         GenericApplicationContext context = new GenericApplicationContext();
         context.refresh();
         context.registerBean("user", UserEntity.class, UserEntity::new);
