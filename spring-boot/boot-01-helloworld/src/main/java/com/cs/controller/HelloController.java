262076724091dbf79253d5f285a57e37e7780dcf
Jim <248578737@qq.com>
2022-05-17 13:19:42 +0800

    spring-boot demo

---
+++ spring-boot/boot-01-helloworld/src/main/java/com/cs/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handler(){
+        return "hello spring-boot";
+    }
+
+}
